0f4cdb5f66ab4eaf5aecd82f4be26adaff2d9575
Marcial Ambriz <mackial.dev@gmail.com>
2022-02-22 21:53:08 -0600

    e

--- src/windows/init.ps1
+++ src/windows/init.ps1
@@ -1,10 +1,10 @@
 # Step 1 ------------------------------------------------
 # Self elevate administrative permissions in this script
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+# if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-function Check-Command($cmdname) {
-  return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
-}
+# function Check-Command($cmdname) {
+#   return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
+# }
 # Activate Windows
 slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
 # Enable WSL 1
